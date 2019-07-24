60cd6e2e0b68f7aaac12f714fefccb5269964cba
Pman143 <semenya143@gmail.com>
2019-07-24 20:24:24 +0200

    View Recipe Item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Chocolate','This is my chocolate','https://cdn.pixabay.com/photo' +
+      '/2016/06/15/19/09/food-1459693_960_720.jpg'), new Recipe('Chocolate 2','This is my chocolate 2','https://cdn.pixabay.com/photo' +
       '/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
