b265acba81e374264d6f2d2718d01e9ed5bb7740
Pman143 <semenya143@gmail.com>
2019-07-29 18:58:55 +0200

    Recipe Service and Shopping- List Service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,15 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+ private recipes: Recipe[] = [
+    new Recipe('Chocolate','This is my chocolate','https://cdn.pixabay.com/photo' +
+      '/2016/06/15/19/09/food-1459693_960_720.jpg'), new Recipe('Chocolate 2','This is my chocolate 2','https://cdn.pixabay.com/photo' +
+      '/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+ getRecipes() {
+   return this.recipes.slice();
+ }
+}
