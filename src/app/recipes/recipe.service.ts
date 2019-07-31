4289e550a7e68a913a59c909e31a930a563c43e2
Pman143 <semenya143@gmail.com>
2019-07-31 15:25:40 +0200

    Recipe Service and Shopping- List Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,8 +4,8 @@ import {EventEmitter} from '@angular/core';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Chocolate', 'This is my chocolate', 'https://cdn.pixabay.com/photo' +
-      '/2016/06/15/19/09/food-1459693_960_720.jpg'), new Recipe('Chocolate 2', 'This is my chocolate 2', 'https://cdn.pixabay.com/photo' +
+    new Recipe('Meat', 'This is my meat', 'https://cdn.pixabay.com/photo' +
+      '/2016/06/15/19/09/food-1459693_960_720.jpg'), new Recipe('Meat 2', 'This is my meat 2', 'https://cdn.pixabay.com/photo' +
       '/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
