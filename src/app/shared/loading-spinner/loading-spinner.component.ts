0ab756774830b518fc988926522dccb9166011bd
Pman143 <semenya143@gmail.com>
2019-08-13 18:36:17 +0200

    Loading Spinner, Authentication

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
-  template: '<div class="lds-dual-ring"></div>',
+  template: '<div style="margin-top: 100px" class="lds-ring"><div></div><div></div><div></div><div></div></div>',
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {
