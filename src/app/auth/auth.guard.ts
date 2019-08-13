c2635cf2562f6e59acf68be43188b978a35076ec
Pman143 <semenya143@gmail.com>
2019-08-13 20:55:04 +0200

    Added the URL Tree On Auth Guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
@@ -9,15 +9,20 @@ import {map} from 'rxjs/operators';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot):
-    Observable<boolean> | Promise<boolean> | boolean {
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean  | UrlTree{
     return this.authService.user.pipe(map(
       user => {
-        return !!user;
+        const isAuth = !!user;
+        if(isAuth){
+          return true;
+        }
+
+        return this.router.createUrlTree(['/auth']);
       }
     ));
   }
